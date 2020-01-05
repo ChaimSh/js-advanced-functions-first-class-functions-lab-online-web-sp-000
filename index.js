df32db5711812fdb80a9568a565773c37291396c
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-05 18:12:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 function returnFirstTwoDrivers() {
   const anonymousFunc = function(dirvers) {
-    return drivers.slice(0, 2);
+    return anonymousFunc.slice(0, 2);
   }
 }
