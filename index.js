5a56bb49b35dc50d9eeccf536d3c7344d4b95d5d
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-05 18:25:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const returnFirstTwoDrivers = function(drivers) {
     return drivers.slice(0, 2);
   }
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2)[0]
+}
+// arr.slice(1).slice(-5)
